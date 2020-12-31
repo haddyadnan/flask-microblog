dfde43b199d51dc2a65b2162e3f8fdcd78d371bb
haddyadnan <haddyadnan@gmail.com>
2020-12-31 15:30:10 +0100

    added profile page functionality

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -6,6 +6,7 @@ from app.forms import RegistrationForm
 from flask_login import login_required, current_user, login_user, logout_user
 from flask import url_for, render_template, flash, redirect, request
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -65,3 +66,20 @@ def register():
         flash('Congratulations, you are a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author':user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
